44c6a865c03e1aff0b3859f4dd553bedf41b8a6e
feherdaniel <feherdaniel2010@gmail.com>
2021-11-17 17:45:11 +0100

    initialize application

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form">
+        <input type="text" name="movieName"/>
+        <input type="text" name="review"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
